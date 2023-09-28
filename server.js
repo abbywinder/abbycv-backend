ecb0bfc66a6cefe60022e16b8b9d90750884d2fc
abbywinder <abbywinder@Abbys-MBP-2.lan>
2023-09-28 18:31:29 +0100

    proxy added

--- server.js
+++ server.js
@@ -2,13 +2,33 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
+const url = require('url');
+
+// FIXIE CONFIG
+let proxy;
+if (process.env.FIXIE_URL) {
+    const fixieUrl = url.parse(process.env.FIXIE_URL);
+    const fixieAuth = fixieUrl.auth.split(':');
+    proxy = {
+        proxyHost: fixieUrl.hostname,
+        proxyPassword: fixieAuth[1],
+        proxyPort: fixieUrl.port,
+        proxyUsername: fixieAuth[0]
+    };
+};
 
 //DB CONFIG
 const port = process.env.PORT || 3003;
-const db = process.env.FIXIE_URL ? process.env.FIXIE_URL + process.env.MONGO_DB_CONNECTION_STR : process.env.MONGO_DB_CONNECTION_STR;
+const db = process.env.MONGO_DB_CONNECTION_STR;
 
 mongoose
-.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+.connect(db, {
+    ...{
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    },
+    ...(proxy ? proxy : {})
+})
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
