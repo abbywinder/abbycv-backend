6801788ca1d421f97ab8ec066dd4daabd2d87299
abbywinder <abbywinder@Abbys-MBP-2.lan>
2023-09-28 15:29:16 +0100

    fixie proxy added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 
 //DB CONFIG
 const port = process.env.PORT || 3003;
-const db = process.env.MONGO_DB_CONNECTION_STR;
+const db = process.env.FIXIE_URL ? process.env.FIXIE_URL + process.env.MONGO_DB_CONNECTION_STR : process.env.MONGO_DB_CONNECTION_STR;
 
 mongoose
 .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
